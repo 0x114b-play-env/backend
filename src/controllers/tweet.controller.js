9a6d02f8603b6e79e3d18a1f0237dc0a0a03d496
0x114b <abracadabra4427@gmail.com>
2025-07-28 15:15:17 +0530

    Implements tweet creation and retrieval
    Adds functionality to create new tweets and retrieve tweets for a specific user.
    It includes input validation and error handling for cases where content is missing during creation or the user ID is missing during retrieval. Also, provides a user-friendly message when no tweets are found for a user.

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -3,18 +3,57 @@ import { Tweet } from "../models/tweet.model.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/AsyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
   //TODO: create tweet
+
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  const userId = req.user._id;
+  const user = await User.findById(userId);
+
+  const tweet = await Tweet.create({
+    owner: user._id,
+    content,
+  });
+
+  if (!tweet) {
+    throw new ApiError(500, "Something went wrong while creating the tweet");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, tweet, "Tweet created successfully"));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
   // TODO: get user tweets
+  const {userId} = req.params;
+
+  if (!userId) {
+        throw new ApiError(400, "user id is missing");
+    
+  }
+
+  const userTweets = await Tweet.find({ owner: userId });
+
+  const message = userTweets.length
+    ? "User tweets fetched successfully"
+    : "No tweets found for user";
+
+  return res.status(200).json(new ApiResponse(200, userTweets, message));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
-  //TODO: update tweet
+  //TODO: update tweet 
+
+  // const tweetToUpdate;
+
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
